40757bfd5e1c185da925c7e70c4d984ab6f84ba8
hetanthakkar <hetanthakkar1@gmail.com>
2024-03-22 21:26:45 -0400

    Class acitivty 18th march

--- pages/authors.js
+++ pages/authors.js
@@ -1,14 +1,16 @@
-let Author = require('../models/author');
+let Author = require("../models/author");
 
 get_author_list = async () => {
-  let authors_list = [];
-  return authors_list.map(function(author) {
-    return author.name + " : " + author.lifespan;
-  });
+  let authors = await Author.find();
+  let authors_list = authors.map((author) => ({
+    name: author.name,
+    lifespan: author.lifespan,
+  }));
+  return authors_list;
 };
 
-exports.show_all_authors = function(res) {
+exports.show_all_authors = function (res) {
   get_author_list()
     .then((data) => res.send(data))
-    .catch((_) => res.send('No authors found'));
-}
+    .catch((_) => res.send("No authors found"));
+};
--- pages/books_status.js
+++ pages/books_status.js
@@ -1,5 +1,20 @@
-let BookInstance = require('../models/bookinstance');
+let BookInstance = require("../models/bookinstance");
 
-exports.show_all_books_status = function(res) {
-  return res.send([]);
-}
+exports.show_all_books_status = function (res) {
+  BookInstance.find({ status: "Available" })
+    .populate("book")
+    .exec(function (err, availableBooks) {
+      if (err) {
+        return res
+          .status(500)
+          .send("Error occurred while fetching book instances");
+      }
+
+      let availableBooksList = availableBooks.map((bookInstance) => ({
+        title: bookInstance.book.title,
+        status: bookInstance.status,
+      }));
+
+      return res.send(availableBooksList);
+    });
+};
